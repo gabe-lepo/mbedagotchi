9da3d47fd1199e7f3df16c7d1051935777ccd8f9
gabe-gardyn <gabriel@gardyn.tech>
2024-11-10 16:58:55 -0500

    troubleshooting

--- blink.c
+++ blink.c
@@ -1,15 +1,28 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define LED_PIN PB5
+#define LED_PIN PORTB5 // 00X0 0000
+#define SETTER (1 << PORTB5)
 
 int main(void)
 {
+   unsigned int counter;
    DDRB |= (1 << LED_PIN);
-   while (1)
+
+   do
    {
-      PORTB ^= (1 << LED_PIN);
-      _delay_ms(1000);
-   }
+      counter = 1;
+      PORTB &= ~(1 << LED_PIN); // Ensure LED bit is off
+      // ~(1 << 5) = 1101 1111
+      //
+
+      PORTB ^= (1 << LED_PIN); // LED BIT FLIP - ON
+      _delay_ms(500);
+      PORTB ^= (1 << LED_PIN); // LED BIT FLIP - OFF
+      _delay_ms(500);
+
+      counter++;
+   } while (counter <= 10);
+
    return 0;
 }
