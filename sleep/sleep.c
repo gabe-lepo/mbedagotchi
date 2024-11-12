286c6c76f239eb33bff0fc241411c286d4c86cd4
gabe-gardyn <gabriel@gardyn.tech>
2024-11-12 12:56:12 -0500

    breakdown of header macros

--- sleep/sleep.c
+++ sleep/sleep.c
@@ -1,9 +1,23 @@
 #include <avr/sleep.h>
+#include <avr/io.h>
+
+int _leftshift(bit)
+{
+   // Arbitration of _BV()
+   return (1 << bit);
+}
 
 int main(void)
 {
-   set_sleep_mode(SLEEP_MODE_IDLE);
-   sleep_enable();
+   // Breakdown of set_sleep_mode()
+   SMCR &= ~(_leftshift(SM0) | _leftshift(SM1) | _leftshift(SM2)); // Clear SMCR bits - This does not trigger any action yet
+   // Might be a good idea to disable interrupts here to prevent any accidental usage of cleared SMCR
+   SMCR |= (SLEEP_MODE_IDLE); // Set IDLE sleep mode
+
+   // Breakdown of sleep_enable()
+   SMCR |= (uint8_t)_leftshift(SE); // Set sleep enable bit
+
+   // sleep_cpu macro is just the AVR sleep instruction, no need to arbitrate
    sleep_cpu();
 
    return 0;
