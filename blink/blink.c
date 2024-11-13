1867f8fc2820c7c50c691728393a5e388c8f5d16
gabe-gardyn <gabriel@gardyn.tech>
2024-11-12 21:58:01 -0500

    fixed infinite counter and added better comments

--- blink/blink.c
+++ blink/blink.c
@@ -1,22 +1,27 @@
 #include <avr/io.h>
 #include <util/delay.h>
+#include <sys/types.h>
 
-#define LED_PIN PORTB5 // 00X0 0000
+#define LED_PIN PORTB5
+#define BLINK_TIME_MS 1000
 
 int main(void)
 {
-   unsigned int counter;
+   unsigned int counter = 1;
+   // Set data direction register for port B pin 5 to OUT
    DDRB |= (1 << LED_PIN);
 
    do
    {
-      counter = 1;
-      PORTB &= ~(1 << LED_PIN); // Ensure LED bit is off
+      // Ensure LED bit is off by AND assigning the inverse of 1<<5
+      PORTB &= ~(1 << LED_PIN);
 
-      PORTB ^= (1 << LED_PIN); // LED BIT FLIP - ON
-      _delay_ms(500);
-      PORTB ^= (1 << LED_PIN); // LED BIT FLIP - OFF
-      _delay_ms(500);
+      // Flip the PORTB pin 5 led bit (it should be 0 after the previous PORTB line)
+      PORTB ^= (1 << LED_PIN);
+      _delay_ms(BLINK_TIME_MS / 2);
+      // Flip the PORTB pin 5 led bit (it should be 1 after the previous PORTB line)
+      PORTB ^= (1 << LED_PIN);
+      _delay_ms(BLINK_TIME_MS / 2);
 
       counter++;
    } while (counter <= 10);
