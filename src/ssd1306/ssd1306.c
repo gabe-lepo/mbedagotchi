d1d834e6572f552d84b60c7885668b99ead1969f
gabe-lepo <gabelepoutre@gmail.com>
2025-07-05 12:32:43 -0400

    reorder init sequence, more comments

--- src/ssd1306/ssd1306.c
+++ src/ssd1306/ssd1306.c
@@ -3,6 +3,10 @@
 #include <util/delay.h>
 
 void ssd1306_init(void) {
+  // -----------------
+  // BASIC RESET SETUP
+  // -----------------
+
   // HW reset, then sleep. Apparently the screen goes into
   // sleep mode after HW reset, so this may be redundant
   spi_hw_reset();
@@ -12,8 +16,12 @@ void ssd1306_init(void) {
   spi_send_command(SSD1306_SET_MEM_ADDR_MODE);
   spi_send_data(SSD1306_MEM_ADDR_MODE_HORIZ);
 
+  // ----------------------
+  // HARDWARE CONFIGURATION
+  // ----------------------
+
   // Reset start line
-  spi_send_command(SSD1306_SET_DISP_START_LINE_RESET);
+  spi_send_command(SSD1306_SET_DISP_START_LINE(0));
 
   // Segment remap (normal orientation)
   spi_send_command(SSD1306_SET_SEGMENT_REMAP_COL_0);
@@ -31,17 +39,15 @@ void ssd1306_init(void) {
 
   // Set COM pin HW config
   spi_send_command(SSD1306_SET_COM_PIN_HW_CONFIG);
-  spi_send_data(SSD1306_COM_PIN_CONFIG_ALTERNATIVE);
+  spi_send_data(SSD1306_COM_PIN_CONFIG_ALTERNATING);
+
+  // --------------
+  // POWER & TIMING
+  // --------------
 
   // Medium contrast
   spi_send_command(SSD1306_SET_CONTRAST_CTRL);
-  spi_send_data(SSD1306_INIT_CONTRAST);
-
-  // Entire disp on with RAM
-  spi_send_command(SSD1306_ALL_DISP_ON_FOLLOW_RAM);
-
-  // Non-inverted mode
-  spi_send_command(SSD1306_SET_NORMAL_DISP);
+  spi_send_data(SSD1306_CONTRAST_MID);
 
   // Display clock divide ratio/osc freq
   spi_send_command(SSD1306_SET_DISP_CLK_DIV_RATIO);
@@ -49,11 +55,25 @@ void ssd1306_init(void) {
 
   // Pre-charge period
   spi_send_command(SSD1306_SET_PRECHARGE_PERIOD);
-  spi_send_data(SSD1306_INIT_PRECHARGE);
+  spi_send_data(SSD1306_PRECHARGE_INIT);
 
   // Set VCOMH deselect level
   spi_send_command(SSD1306_SET_VCOMH_DESELECT_LVL);
-  spi_send_data(SSD1306_INIT_VCOMH);
+  spi_send_data(SSD1306_VCOMH_DESELECT_077_VCC);
+
+  // ----------------
+  // DISPLAY BEHAVIOR
+  // ----------------
+
+  // Entire disp on with RAM
+  spi_send_command(SSD1306_ALL_DISP_ON_FOLLOW_RAM);
+
+  // Non-inverted mode
+  spi_send_command(SSD1306_SET_NORMAL_DISP);
+
+  // ------------------
+  // DISPLAY ADDRESSING
+  // ------------------
 
   // Set up column and page address ranges (full screen)
   spi_send_command(SSD1306_SET_COL_ADDR);
@@ -64,8 +84,11 @@ void ssd1306_init(void) {
   spi_send_data(SSD1306_PAGE_ADDR_START_RESET);
   spi_send_data(SSD1306_PAGE_ADDR_END_RESET);
 
+  // ------------
+  // Done, run it
+  // ------------
+
   // Display on
   spi_send_command(SSD1306_SET_DISP_AWAKE);
-
   _delay_ms(100);
 }
